e7755461303cf79375c8ec67162efa5bd999f7f4
Abdul Mannan KHAN <Abdul.Mannan.KHAN@soneparindia.com>
2022-04-21 10:23:22 +0530

    akratechTask

---
+++ src/Root.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import "./Root.css";
+import * as axios from "axios";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Appbar from "./components/Appbar";
+import Card from "./components/Card";
+
+function Root() {
+  const [usersData, setUsersData] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+
+  function handleRefresh() {
+    setRefresh(!refresh);
+    setLoader(true);
+  }
+
+  useEffect(
+    function () {
+      localStorage.clear();
+      axios
+        .get("https://randomuser.me/api/?results=50")
+        .then((result) => {
+          const data = result.data.results.map((user, index) => {
+            return {
+              name: user.name.first + " " + user.name.last,
+              picture: user.picture.large,
+              gender: user.gender,
+              address: user.location.city,
+              id: index,
+            };
+          });
+          // console.log(data);
+          data.map((user, index) => {
+            localStorage.setItem(user.id, JSON.stringify(user));
+          });
+          setLoader(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    [refresh]
+  );
+
+  function deleteUser(id) {
+    console.log(id);
+    localStorage.removeItem(id);
+  }
+
+  useEffect(
+    function () {
+      function allStorage() {
+        var archive = [];
+        for (var i = 0; i < localStorage.length; i++) {
+          archive[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        }
+        return archive;
+      }
+      // console.log(allStorage());
+      setUsersData(allStorage);
+    },
+    [loader, deleteUser]
+  );
+
+  console.log(usersData);
+  return (
+    <div className="App">
+      <Appbar refresh={handleRefresh} />
+      {loader && (
+        <Box
+          sx={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+
+      {!loader && (
+        <>
+          <div className="total-users">
+            <h1>Total Users: {usersData.length}</h1>
+          </div>
+          <div className="card-container">
+            {usersData.map((user) => {
+              return (
+                <Card
+                  name={user.name}
+                  gender={user.gender}
+                  address={user.address}
+                  picture={user.picture}
+                  id={user.id}
+                  key={user.id}
+                  deleteUser={deleteUser}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Root;
